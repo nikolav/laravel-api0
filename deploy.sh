dfa70dbb76e3c6ee62f36d7a8664d6a1f15b8228
Vukovic Nikola <admin@nikolav.rs>
2026-01-21 17:11:24 +0100

    Wed, Jan 21, 2026  4:11:24 PM

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@ docker compose down
 docker compose build --no-cache
 docker compose up -d api
 docker compose logs -f api
+
+# docker stop $(docker ps -q) 2>/dev/null; docker system prune -a --volumes -f
