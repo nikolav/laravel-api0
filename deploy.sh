89fef3c2bb1d14c3e2dba50d4623ea6dc432ee3e
Vukovic Nikola <admin@nikolav.rs>
2026-01-21 23:26:23 +0100

    Wed, Jan 21, 2026 10:26:23 PM

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,6 @@ docker compose up -d --force-recreate
 # show a quick status
 docker compose ps
 docker compose logs --tail=80 api
+
+# docker compose down -v --rmi all --remove-orphans
+# docker system prune --all --volumes --force
