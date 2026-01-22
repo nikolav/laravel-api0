ff0e231d66a431b27c38bcd0ff90774ee56af977
Vukovic Nikola <admin@nikolav.rs>
2026-01-22 23:23:08 +0100

    Thu, Jan 22, 2026 10:23:08 PM

--- config/mail.php
+++ config/mail.php
@@ -47,6 +47,10 @@ return [
       'password' => env('MAIL_PASSWORD'),
       'timeout' => null,
       'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+      //
+      // Auto-queue all SMTP emails
+      'queue' => 'emails',
+
     ],
 
     'ses' => [
