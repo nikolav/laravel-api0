cd1e528d5da4515321bcb75e57b8ecc9dd63abc6
Vukovic Nikola <admin@nikolav.rs>
2026-01-25 04:46:32 +0100

    Sun, Jan 25, 2026  3:46:32 AM

--- deploy-pull.sh
+++ deploy-pull.sh
@@ -2,26 +2,22 @@
 set -euo pipefail
 
 docker pull 0imbn7v6rkw/laravel-api0 && \
-docker stop laravel-api || true && \
-docker rm laravel-api || true && \
 docker run -d \
-  --name laravel-api \
   -p 127.0.0.1:9000:9000 \
   --env-file ./.env \
   -e APP_ENV=production \
   -e APP_DEBUG=false \
-  -e RUN_MIGRATION=true \
-  -e CACHE_ARTISAN=true \
   -e LOG_CHANNEL=stderr \
+  -e LOG_LEVEL=info \
   -e CACHE_STORE=redis \
   -e SESSION_DRIVER=redis \
-  -e QUEUE_CONNECTION=redis \
+  -e RUN_MIGRATION=false \
+  -e CACHE_ARTISAN=true \
   --pull=always \
   --restart unless-stopped \
   0imbn7v6rkw/laravel-api0
 
 docker ps -a
-docker logs --tail=122 laravel-api
 
 # docker compose down -v --rmi all --remove-orphans
 # docker system prune --all --volumes --force
