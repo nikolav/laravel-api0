207b86a35244d2cf24f99b03a08b904d971ba8a1
Vukovic Nikola <admin@nikolav.rs>
2026-01-27 15:39:00 +0100

    aws setup

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,16 @@ use App\Http\Controllers\TestingController;
 use App\Http\Controllers\WebhookHandleController;
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
+use Aws\Sdk as AwsSdk;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-  Route::get('/', fn() => response()->json(['status' => 'ok']))
-    ->name('api:status');
+  Route::get(
+    '/',
+    fn() => response()->json([
+      'status'  => 'ok',
+      'aws-sdk' => AwsSdk::VERSION,
+    ])
+  )->name('api:status');
 
   if (!app()->environment('production')) {
     Route::post('/testing', [TestingController::class, 'demo'])
