5fc75774d52d58b82103dda83f06d5b1eedc2af5
Vukovic Nikola <admin@nikolav.rs>
2026-01-29 22:00:28 +0100

    Thu, Jan 29, 2026  9:00:28 PM

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-  App\Providers\AppServiceProvider::class,
-  App\Providers\RateLimitersProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\EventsManualProvider::class,
+    App\Providers\RateLimitersProvider::class,
 ];
