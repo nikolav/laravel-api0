1148f916af8fc6c7bf6fc70e7174c9b2f31b3156
Vukovic Nikola <admin@nikolav.rs>
2026-01-31 14:21:36 +0100

    Sat, Jan 31, 2026  1:21:36 PM

---
+++ app/Providers/ViewComposerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+// use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewComposerServiceProvider extends ServiceProvider
+{
+  /**
+   * Register services.
+   */
+  public function register(): void
+  {
+    //
+  }
+
+  /**
+   * Bootstrap services.
+   */
+  public function boot(): void
+  {
+    //
+    // View::composer('partials.sidebar', function ($view) {
+    //   $view->with('data', collect(['data'])->toArray());
+    // });
+  }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\EventsManualProvider::class,
-    App\Providers\RateLimitersProvider::class,
+  App\Providers\AppServiceProvider::class,
+  App\Providers\EventsManualProvider::class,
+  App\Providers\RateLimitersProvider::class,
+  App\Providers\ViewComposerServiceProvider::class,
 ];
