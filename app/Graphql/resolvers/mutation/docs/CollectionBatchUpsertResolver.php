20ba4f1345cabb974a53fc0433d043ff54dac4f0
Vukovic Nikola <admin@nikolav.rs>
2026-02-14 13:01:20 +0100

    Sat, Feb 14, 2026 12:01:20 PM

--- app/Graphql/resolvers/mutation/docs/CollectionBatchUpsertResolver.php
+++ app/Graphql/resolvers/mutation/docs/CollectionBatchUpsertResolver.php
@@ -72,11 +72,7 @@ class CollectionBatchUpsertResolver
           // ==inserts
           if ($withoutId->isNotEmpty()) {
             // tag:id to insert docs under
-            $tid = Tags::where(['tag' => $tag])->first()?->id;
-            if (empty($tid)) {
-              // no tag with that name, create
-              $tid = Tags::create(['tag' => $tag])->id;
-            }
+            $tid = Tags::firstOrCreate(['tag' => $tag])->id;
 
             $inserts = $withoutId->map(function ($p) use ($now) {
               $p = (array) $p;
