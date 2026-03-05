4a0160378caeef07d22e2c2c3fb410ecc106ce81
Vukovic Nikola <admin@nikolav.rs>
2026-03-05 23:52:15 +0100

    Thu, Mar  5, 2026 10:52:14 PM

--- app/Enums/AssetsType.php
+++ app/Enums/AssetsType.php
@@ -10,20 +10,23 @@ enum AssetsType: string
   case People_Group         = '3f21dedb-3b37-5baa-82e0-0610fafed17d';
   case People_Employee      = 'a28b1277-21a5-597b-9f70-a889496ebfe3';
   case People_Person        = 'b51e7306-fa19-55a3-a1a2-fe039d4e8619';
+    // authorization
+  case Security_Policy      = 'fddd790d-f056-56c9-9437-2931bf8c7c31';
+  case Security_Role        = 'e99db6ff-7f43-5860-ab0d-3a1485ed25c6';
+  case Security_Permission  = '84ccba4e-a9df-55ec-b5e0-c97099129718';
+    // media
+  case Media_Image          = 'ff9e31f3-ba7d-592d-a738-0c6370746653';
+    // stores
+  case Order                = 'd5907216-8ad1-5548-8a51-fc09e1bddd1d';
     // physical
   case Physical_Product     = '61a710e5-7fd9-58f2-aaa7-11d16a276e76';
   case Physical_Store       = '34f10445-6150-59e4-93e2-a716ac782d78';
   case Physical_Document    = '74063c22-a8ab-55f5-9b17-f8e3079fea73';
   case Physical_Service     = '36594c9c-404a-5427-a559-b5a67b68238a';
-    // authorization
-  case Security_Policy      = 'fddd790d-f056-56c9-9437-2931bf8c7c31';
-  case Security_Role        = 'e99db6ff-7f43-5860-ab0d-3a1485ed25c6';
-  case Security_Permission  = '84ccba4e-a9df-55ec-b5e0-c97099129718';
     // digital
   case Digital_Post         = '85f212e1-6cd3-55e0-9d09-bfbcf751fe88';
   case Digital_Comment      = 'cbce4939-7d95-5f9c-a90d-9f1b2954ddfe';
-    // media
-  case Media_Image          = 'ff9e31f3-ba7d-592d-a738-0c6370746653';
+
 
   function label(): string
   {
@@ -42,6 +45,7 @@ enum AssetsType: string
       self::Security_Permission => 'Security:Permission',
       self::Media_Image         => 'Media:Image',
       self::Category            => 'Category',
+      self::Order               => 'Order',
     };
   }
 }
