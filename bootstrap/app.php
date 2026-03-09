64d7f8aea2a3a197212f2992602953b89b298756
Vukovic Nikola <admin@nikolav.rs>
2026-03-09 13:24:52 +0100

    Mon, Mar  9, 2026 12:24:52 PM

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,10 @@ use App\Http\Middleware\InternalAuthHttpMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
   ->withRouting(
     web: __DIR__ . '/../routes/web.php',
-    api: __DIR__ . '/../routes/api.php',
+    api: [
+      __DIR__ . '/../routes/api.php',
+      // __DIR__ . '/../routes/other.api.php',
+    ],
     commands: __DIR__ . '/../routes/console.php',
     channels: __DIR__ . '/../routes/channels.php',
     health: '/up',
