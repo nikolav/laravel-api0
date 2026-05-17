1fda885db481caa9d6b713bb1af0c20bb8f20aa4
Vukovic Nikola <admin@nikolav.rs>
2026-05-17 05:54:07 +0200

    Sun, May 17, 2026  3:54:07 AM

--- app/Http/Controllers/TestingController.php
+++ app/Http/Controllers/TestingController.php
@@ -4,17 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Helpers\AppUtils;
 
-// // use App\Mail\DemoMailMessage;
-// use App\Mail\MessagePlainEmail;
-// // use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Mail;
-
 class TestingController extends Controller
 {
   //
   function demo()
   {
-    //
     // $res =
     //   Mail::to(['admin@nikolav.rs'])
     //   ->send(new MessagePlainEmail(
@@ -25,9 +19,6 @@ class TestingController extends Controller
     //     ]
     //   ));
 
-
-    // $auth_firebase = app('firebase.auth');
-    // $user = $auth_firebase->getUser('');
 
     return AppUtils::res([
       'status' => 'ok',
